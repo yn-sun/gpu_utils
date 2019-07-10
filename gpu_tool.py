c731cfc3631a8de82f57228f8b1f0df83045df86
yn-sun <47969757+yn-sun@users.noreply.github.com>
2019-07-10 14:14:57 +0800

    new file

---
+++ gpu_tool.py
@@ -0,0 +1,48 @@
+from subprocess import Popen, PIPE
+import os
+
+class GPUTools(object):
+    
+    @classmethod
+    def _get_equipped_gpu_ids_and_used_gpu_info(cls):
+        p = Popen('nvidia-smi', stdout=PIPE)
+        output_info = p.stdout.read().decode('UTF-8')
+        lines = output_info.split(os.linesep)
+        equipped_gpu_ids = []
+        for line_info in lines:
+            if not line_info.startswith(' '):
+                if 'GeForce' in line_info:
+                    equipped_gpu_ids.append(line_info.strip().split(' ', 4)[3])
+            else:
+                break
+        
+        gpu_info_list = []
+        for line_no in range(len(lines)-3, -1, -1):
+            if lines[line_no].startswith('|==='):
+                break
+            else:
+                gpu_info_list.append(lines[line_no][1:-1].strip())
+                
+        return equipped_gpu_ids, gpu_info_list
+    
+
+   
+    @classmethod
+    def detect_availabel_gpu_id(cls):
+        equipped_gpu_ids, gpu_info_list = cls._get_equipped_gpu_ids_and_used_gpu_info()
+        
+        used_gpu_ids = []
+        
+        for each_used_info in gpu_info_list:
+            if 'python' in each_used_info:
+                used_gpu_ids.append((each_used_info.strip().split(' ', 1)[0]))
+        
+        unused_gpu_ids = []
+        for id_ in equipped_gpu_ids:
+            if id_ not in used_gpu_ids:
+                unused_gpu_ids.append(id_)
+                
+        return unused_gpu_ids      
+    
+    
+    
